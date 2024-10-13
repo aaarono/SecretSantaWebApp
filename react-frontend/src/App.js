58d2ac120cf22c63547b87e6661832bd8f148cc1
Andrii Streblichenko <vojavy@gmail.com>
2024-10-13 21:11:05 +0200

    registration form

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage'
 import LoginForm from './pages/LoginPage';
+import RegistrationForm from './pages/RegistrationPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/login" element={<LoginForm />} />
-      <Route path="/registration" element={<MainPage />} />
+      <Route path="/registration" element={<RegistrationForm />} />
     </Routes>
   );
 }
---
+++ react-frontend/src/pages/RegistrationPage.js
@@ -0,0 +1,134 @@
+import React, { useState } from 'react';
+import Button from '../components/ui/Button';
+import TextInput from '../components/ui/TextInput';
+import RadioButtons from '../components/ui/RadioButtons';
+import '../index.css';
+
+const RegistrationForm = () => {
+  const [showErrors, setShowErrors] = useState(false);
+  const [formValues, setFormValues] = useState({
+    email: '',
+    username: '',
+    phone: '',
+    name: '',
+    surname: '',
+    password: '',
+    passwordRepeat: '',
+  });
+
+  const styles = {
+    centerContainer: {
+      textAlign: 'center',
+      display: 'flex',
+      gap: '10px',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+  };
+
+  const validateEmail = (email) => {
+    return email.includes('@');
+  };
+
+  const validateUsername = (username) => {
+    return username.length >= 3;
+  };
+
+  const validatePhone = (phone) => {
+    return phone.length >= 10;
+  };
+
+  const validatePassword = (password) => {
+    return password.length >= 6;
+  };
+
+  const validateSecondPassword = (passwordRepeat) => {
+    return passwordRepeat === formValues.password;
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setShowErrors(true);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  return (
+    <form className='section-container' onSubmit={handleFormSubmit}>
+      <div style={styles.centerContainer}>
+        <h1>Registration</h1>
+        <TextInput 
+          name="email" 
+          type='email' 
+          placeholder="E-mail" 
+          errorCheck={showErrors ? validateEmail : () => true} 
+          errorText="Email must be valid" 
+          value={formValues.email}
+          onChange={handleInputChange}
+        />
+        <TextInput 
+          name="username" 
+          placeholder="Username" 
+          errorCheck={showErrors ? validateUsername : () => true} 
+          errorText="Username must be at least 3 characters long" 
+          value={formValues.username}
+          onChange={handleInputChange}
+        />
+        <TextInput 
+          name="phone" 
+          type='tel' 
+          placeholder="Phone" 
+          errorCheck={showErrors ? validatePhone : () => true} 
+          errorText="Phone must be valid" 
+          value={formValues.phone}
+          onChange={handleInputChange}
+        />
+        <TextInput 
+          name="name" 
+          placeholder="Name" 
+          errorCheck={showErrors ? validateUsername : () => true} 
+          errorText="Name must be at least 3 characters long" 
+          value={formValues.name}
+          onChange={handleInputChange}
+        />
+        <TextInput 
+          name="surname" 
+          placeholder="Surname" 
+          errorCheck={showErrors ? validateUsername : () => true} 
+          errorText="Surname must be at least 3 characters long" 
+          value={formValues.surname}
+          onChange={handleInputChange}
+        />
+        <RadioButtons 
+          options={{ Male: 'male', Female: 'female' }} 
+          direction='horizontal'
+          style={{ marginBottom: '10px' }}
+        />
+        <TextInput 
+          name="password" 
+          placeholder="Password" 
+          type='password' 
+          errorCheck={showErrors ? validatePassword : () => true} 
+          errorText="Password must be at least 6 characters long"
+          value={formValues.password}
+          onChange={handleInputChange}
+        />
+        <TextInput 
+          name="passwordRepeat" 
+          placeholder="Repeat password" 
+          type='password' 
+          errorCheck={showErrors ? validateSecondPassword : () => true} 
+          errorText="Passwords must match" 
+          value={formValues.passwordRepeat}
+          onChange={handleInputChange}
+        />
+        <Button text="Register" type="submit" />
+      </div>
+    </form>
+  );
+};
+
+export default RegistrationForm;
