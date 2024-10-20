f1d1cd79f180490f4114f9580fec7e4b857dcc48
Andrii Streblichenko <vojavy@gmail.com>
2024-10-20 14:35:08 +0200

    Новый бэк. Перед пулом макар напиши

--- php-backend/app/skeleton/config/bundles.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true, 'test' => false],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-];
