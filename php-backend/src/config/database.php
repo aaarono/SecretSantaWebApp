5cd6ae1bd2b5bd2da44bc52c54fed77071a23eac
Oleksandr <120096594+aaarono@users.noreply.github.com>
2024-12-19 19:14:10 +0100

    db env 2

--- php-backend/src/config/database.php
+++ php-backend/src/config/database.php
@@ -11,20 +11,30 @@ class Database {
         $this->conn = null;
 
         try {
-            // Получаем параметры подключения из переменных окружения
-            $host = getenv('PGHOST');
-            $port = getenv('PGPORT');
-            $db_name = getenv('PGDATABASE');
-            $username = getenv('PGUSER');
-            $password = getenv('PGPASSWORD');
+            // Получаем строку подключения из переменной окружения
+            $databaseUrl = getenv('DATABASE_URL');
+
+            if (!$databaseUrl) {
+                throw new \Exception('DATABASE_URL переменная окружения не найдена');
+            }
+
+            // Парсим строку подключения
+            $dbParts = parse_url($databaseUrl);
+
+            $host = $dbParts['host'];
+            $port = $dbParts['port'];
+            $user = $dbParts['user'];
+            $password = $dbParts['pass'];
+            $dbname = ltrim($dbParts['path'], '/');
 
             // Формируем строку подключения для PostgreSQL
-            $dsn = "pgsql:host=$host;port=$port;dbname=$db_name";
-            $this->conn = new PDO($dsn, $username, $password);
+            $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+            $this->conn = new PDO($dsn, $user, $password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (\PDOException $exception) {
-            // Выводим сообщение об ошибке, если не удалось подключиться
-            echo "Connection error: " . $exception->getMessage();
+            echo "Ошибка подключения: " . $exception->getMessage();
+        } catch (\Exception $exception) {
+            echo "Общая ошибка: " . $exception->getMessage();
         }
 
         return $this->conn;
