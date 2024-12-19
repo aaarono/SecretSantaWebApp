2a150cbfe33913c09c1d2c6baf849f8845113c68
Oleksandr <120096594+aaarono@users.noreply.github.com>
2024-12-20 00:54:22 +0100

    123

--- php-backend/src/config/database.php
+++ php-backend/src/config/database.php
@@ -5,34 +5,24 @@ namespace Secret\Santa\Config;
 use PDO;
 
 class Database {
-    private $host;
-    private $port;
-    private $db_name;
-    private $username;
-    private $password;
+    private $host = '20.126.41.73'; // Хост базы данных (например, имя сервиса Docker или IP-адрес)
+    private $port = '65433'; // Порт PostgreSQL
+    private $db_name = 'postgres'; // Имя базы данных
+    private $username = 'santa'; // Имя пользователя базы данных
+    private $password = 'santa_password!'; // Пароль базы данных
     public $conn;
-
-    public function __construct() {
-        // Получение параметров из переменных окружения
-        $this->host = getenv('DB_HOST');
-        $this->port = getenv('DB_PORT') ?: '5432';
-        $this->db_name = getenv('DB_NAME');
-        $this->username = getenv('DB_USER');
-        $this->password = getenv('DB_PASSWORD');
-    }
 
     public function getConnection() {
         $this->conn = null;
 
         try {
             // Формируем строку подключения для PostgreSQL
-            $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name};sslmode=require";
+            $dsn = "pgsql:host=$this->host;port=$this->port;dbname=$this->db_name";
             $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (\PDOException $exception) {
-            // Логирование ошибки вместо прямого вывода
-            error_log("Connection error: " . $exception->getMessage());
-            // Можно выбросить исключение или обработать ошибку другим способом
+            // Выводим сообщение об ошибке, если не удалось подключиться
+            echo "Connection error: " . $exception->getMessage();
         }
 
         return $this->conn;
--- react-frontend/src/services/api/api.js
+++ react-frontend/src/services/api/api.js
@@ -1,5 +1,5 @@
 import ApiService from './ApiService';
 
-const api = new ApiService(process.env.REACT_APP_BACKEND_URL);
+const api = new ApiService('http://20.126.41.73:8000');
 
 export default api;
